bdba4705aceabf2d8339feffc8d33cbaad177cfd
Alex Seitsinger <16756928+alexseitsinger@users.noreply.github.com>
2019-05-23 09:28:32 +0000

    refactor: updates to funcs

--- src/index.js
+++ src/index.js
@@ -1,39 +1,66 @@
+import "core-js/stable"
 import React from "react"
-import { Route, Switch } from "react-router-dom"
+// import { Route, Switch } from "react-router"
 import _ from "underscore"
 
-
 function addRoutes(
-	children,
-	Component,
+	Route,
 	basePath,
+	Component,
+	children,
+	routeProps,
 	mainRoutes,
 	modalRoutes
 ){
-	React.Children.forEach(children, (child) => {
-		var childPath = child.props.path
+	children.forEach((arr) => {
+		var childPath = arr[0]
 		if(!childPath.startsWith(basePath)){
 			childPath = `${basePath}/${childPath}`.replace(/(\/)\/+/g, "$1")
 		}
 		if(childPath === "/*"){
 			childPath = "*"
 		}
+		const ChildComponent = arr[1]
+		var modal
+		var children
+		if(arr.length === 4){
+			modal = arr[2]
+			children = arr[3]
+		}
+		else if (arr.length === 3){
+			if(_.isBoolean(arr[2])){
+				modal = arr[2]
+				children = []
+			}
+			else if (_.isArray(arr[2])){
+				modal = false
+				children = arr[2]
+			}
+		}
+		else {
+			modal = false
+			children = []
+		}
 		const childRoute = (
 			<Route
 				key={"childRoute" + _.uniqueId()}
 				path={childPath}
 				exact
-				component={child.props.component}
+				render={(props) => {
+					return <ChildComponent {...routeProps} {...props}/>
+				}}
 			/>
 		)
-		if(child.props.modal){
+		if(modal){
 			modalRoutes.push(childRoute)
 			mainRoutes.push(
 				<Route
 					key={"mainRoute" + _.uniqueId()}
 					path={childPath}
 					exact
-					component={Component}
+					render={(props) => {
+						return <Component {...routeProps} {...props}/>
+					}}
 				/>
 			)
 		}
@@ -41,32 +68,39 @@ function addRoutes(
 			mainRoutes.push(childRoute)
 		}
 		addRoutes(
-			child.props.children,
-			child.props.component,
+			Route,
 			childPath,
+			ChildComponent,
+			children,
+			routeProps,
 			mainRoutes,
 			modalRoutes
 		)
 	})
 }
 
-function makeRoutes(tree){
-	const root = React.Children.only(tree)
-	const { children, component, path } = root.props
-	const indexRoute = (
+function makeRoutes(Route, arr, routeProps){
+	const root = arr[0]
+	const path = root[0]
+	const Component = root[1]
+	const children = root[2]
+	const mainRoutes = [
 		<Route
 			key={"indexRoute" + _.uniqueId()}
 			path={path}
 			exact
-			component={component}
+			render={(props) => {
+				return <Component {...routeProps} {...props}/>
+			}}
 		/>
-	)
-	const mainRoutes = [ indexRoute ]
+	]
 	const modalRoutes = []
 	addRoutes(
-		children,
-		component,
+		Route,
 		path,
+		Component,
+		children,
+		routeProps,
 		mainRoutes,
 		modalRoutes
 	)
@@ -82,14 +116,28 @@ function makeRoutes(tree){
 	}
 }
 
-export default (tree) => {
-	const { mainRoutes, modalRoutes } = makeRoutes(tree)
-	return (
-		<React.Fragment>
-			<Switch>
-				{mainRoutes}
-			</Switch>
-			{modalRoutes}
-		</React.Fragment>
-	)
+// ***NOTE***
+// Since React-Router & React-Router-DOM use a context object.
+// Using this package multiple times uses multiple context objects
+// and therefore causes React to throw an invariant error when
+// attempting to mix them. As a result, we (sadly) have to pass the
+// components we want to use (Route, Switch) from the calling scope
+// in order to ensure we use the same context. It's also important to
+// avoid using these components (Route, Switch) outside of a router in
+// our calling scope too. As a result, we have t pass an array of 'routes'
+// to dynamically generate them at runtime. The component that this
+// function generates should be the single component used in a route
+// inside the <App/> component.
+export default (Switch, Route, arr) => {
+	return (props) => {
+		const { mainRoutes, modalRoutes } = makeRoutes(Route, arr, props)
+		return (
+			<React.Fragment>
+				<Switch>
+					{mainRoutes}
+				</Switch>
+				{modalRoutes}
+			</React.Fragment>
+		)
+	}
 }
