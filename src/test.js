55f4a33272cdce3c4cde5c0269a374c4b4fb0cd2
Alex Seitsinger <16756928+alexseitsinger@users.noreply.github.com>
2019-05-23 09:29:09 +0000

    test: adds tests for default render

--- src/test.js
+++ src/test.js
@@ -1,11 +1,45 @@
 import React from "react"
+import { MemoryRouter } from "react-router"
+import { Route } from "react-router-dom"
+import createRoutes from "./index"
 
-function setup() {
-	return mount()
-}
+const setup = (path, Component) => mount(
+	<MemoryRouter initialEntries={[path]}>
+		<Route component={Component}/>
+	</MemoryRouter>
+)
+const About = () => <div>About</div>
+const Index = () => <div>Index</div>
+const NotFound = () => <div>Not Found</div>
 
-describe("<NAME/>", () => {
-	it("renders", () => {
-		//...
+describe("<Routes/>", () => {
+	it("renders index", () => {
+		const wrapper = setup("/", createRoutes([
+			["/", Index, [
+				["/about", About]
+			]]
+		]))
+		expect(wrapper.find(Index)).toHaveLength(1)
 	})
+	// it("renders about", () => {
+	// 	const wrapper = setup("/about", (
+	// 		<Routes>
+	// 			<Route path={"/"} component={Index}>
+	// 				<Route path={"/about"} component={About}/>
+	// 			</Route>
+	// 		</Routes>
+	// 	))
+	// 	expect(wrapper.find(About)).toHaveLength(1)
+	// })
+	// it("renders wildcard for unmatching paths", () => {
+	// 	const wrapper = setup("/fdsafsdfadsfsd", (
+	// 		<Routes>
+	// 			<Route path={"/"} component={Index}>
+	// 				<Route path={"/about"} component={About}/>
+	// 				<Route path={"*"} component={NotFound}/>
+	// 			</Route>
+	// 		</Routes>
+	// 	))
+	// 	expect(wrapper.find(NotFound)).toHaveLength(1)
+	// })
 })
