5412325b9c71b0937f46f3466f3cc09d628bb628
Alex Seitsinger <16756928+alexseitsinger@users.noreply.github.com>
2019-06-03 13:19:55 -0400

    refactor: Updates reportRoutes log message.

--- src/createRouteComponent.js
+++ src/createRouteComponent.js
@@ -53,9 +53,31 @@ import { generateRoutes } from "./generateRoutes"
  *
  * export default App
  */
-export function createRouteComponent({ Switch, Route, config }) {
+export function createRouteComponent({ Switch, Route, config, reportRoutes = false }) {
   return function RouteComponent(rootProps) {
     const { mainRoutes, modalRoutes } = generateRoutes({ config, rootProps, Route })
+    if(reportRoutes === true){
+      const mainRoutePaths = mainRoutes.map((route, i) => {
+        const number = (i + 1)
+        const path = route.props.path
+        const exact = route.props.exact ? route.props.exact : false
+        return `${number}) ${path}, ${exact}`
+      }).join("\n  ")
+      const modalRoutePaths = modalRoutes.map((route, i) => {
+        const number = (i + 1)
+        const path = route.props.path
+        const exact = route.props.exact ? route.props.exact : false
+        return `${number}) ${path}, ${exact}`
+      }).join("\n  ")
+      const message = [
+        "[react-nested-routes]",
+        "Main Routes",
+        ("  " + (mainRoutePaths.length ? mainRoutePaths : "None")),
+        "Modal Routes",
+        ("  " + (modalRoutePaths.length ? modalRoutePaths : "None")),
+      ].join("\n")
+      console.log(message)
+    }
     return (
       <React.Fragment>
         <Switch>
